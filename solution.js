309f12c82dcb1b56238d98e695d59ada2c081fdc
Abhishek4298 <59391928+Abhishek4298@users.noreply.github.com>
2020-01-23 12:53:11 +0530

    update operation

--- solution.js
+++ solution.js
@@ -1,22 +1,15 @@
 var mongo = require('mongodb').MongoClient
 const process = require('process');
-//const age = parseInt(process.argv[2]);
-const firstName = process.argv[2];
-const lastName = process.argv[3];
+
 
 let url = 'mongodb://localhost:27017/learnyoumongo';
-	const obj = {
-        firstName : firstName,
-        lastName : lastName
-};
+	
 
 	mongo.connect(url, function(err, db) {
 	let dbo = db.db("learnyoumongo");
-	dbo.collection("parrots").insert(obj,function(err) {
-	if (err) throw err;
-	console.log(JSON.stringify(obj));
-
-})
+	dbo.collection('users').update({ username: 'tinatime' }, { $set: { age: 40 } }).toArray((err) => {
+		if (err) throw err;
+	})
 db.close();
 })
  
