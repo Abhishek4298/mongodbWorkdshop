b939f50bcf62904515987bf02f80c2f7a51d2617
Abhishek4298 <59391928+Abhishek4298@users.noreply.github.com>
2020-01-23 13:19:34 +0530

    count implemented

--- solution.js
+++ solution.js
@@ -1,15 +1,14 @@
 var mongo = require('mongodb').MongoClient
 const process = require('process');
-
-
+const age = parseInt(process.argv[2]);
 let url = 'mongodb://localhost:27017/learnyoumongo';
 	
 
 	mongo.connect(url, function(err, db) {
 	let dbo = db.db("learnyoumongo");
-  dbo.collection(process.argv[3]).remove({ _id: process.argv[4] }).toArray((err) => {
-		if (err) throw err;
-		dbo.close();
-	}); 
-})
- 
+  dbo.collection('parrots').count({age : {$gt: age}}, function (err,documents){
+    if(err) throw err;
+    console.log(documents);
+  })
+ db.close();
+});
