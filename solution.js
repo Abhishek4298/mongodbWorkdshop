2a02fb36f860ef66f6e8f67cf9f916dbf809ec39
Abhishek4298 <59391928+Abhishek4298@users.noreply.github.com>
2020-01-23 12:43:00 +0530

    insert operation

--- solution.js
+++ solution.js
@@ -1,12 +1,22 @@
 var mongo = require('mongodb').MongoClient
 const process = require('process');
-const age = parseInt(process.argv[2]);
-let url = "mongodb://localhost:27017/learnyoumongo";
-mongo.connect(url, function(err, db) {
-let dbo = db.db("learnyoumongo");
-(dbo.collection("parrots").find({age:{$gt:+age}})).toArray((err, res) => {
-if (err) throw err;
-console.log(res);
-dbo.close();
+//const age = parseInt(process.argv[2]);
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+
+let url = 'mongodb://localhost:27017/learnyoumongo';
+	const obj = {
+        firstName : firstName,
+        lastName : lastName
+};
+
+	mongo.connect(url, function(err, db) {
+	let dbo = db.db("learnyoumongo");
+	dbo.collection("parrots").insert(obj,function(err) {
+	if (err) throw err;
+	console.log(JSON.stringify(obj));
+
 })
+db.close();
 })
+ 
